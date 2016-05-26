5d354187de4fcbd53b008e3cce1029779c94d680
VESPT <vesp1989@gmail.com>
2016-05-26 22:34:50 +0900

    cannyエッジをかける

---
+++ app/src/main/java/vesp/howdybrain/MainActivity.java
@@ -0,0 +1,165 @@
+package vesp.howdybrain;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener {
+    //ImageView imgv;
+    private static final String TAG = "MainActivity"; // Debug TAG
+    private Mat mOutputFrame;
+
+    // Initialize OpenCV
+    static {
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "OpenCV not loaded");
+        } else {
+            Log.d(TAG, "OpenCV loaded");
+        }
+    }
+
+    // カメラビューのインスタンス
+    // CameraBridgeViewBase は JavaCameraView/NativeCameraView のスーパークラス
+    private CameraBridgeViewBase mCameraView;
+
+    // ライブラリ初期化完了後に呼ばれるコールバック (onManagerConnected)
+    // public abstract class BaseLoaderCallback implements LoaderCallbackInterface
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                // 読み込みが成功したらカメラプレビューを開始
+                case LoaderCallbackInterface.SUCCESS:
+                    mCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        // OpenCV
+        // カメラビューのインスタンスを変数にバインド
+        mCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+        // リスナーの設定 (後述)
+        mCameraView.setCvCameraViewListener(this);
+        /*
+        Mat mat = new Mat(20,20, CvType.CV_8UC3,new Scalar(0, 0, 255));
+        Bitmap bitmap= Bitmap.createBitmap(mat.width(), mat.height(), Bitmap.Config.ARGB_8888);
+
+        Utils.matToBitmap(mat, bitmap);
+        ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        iv.setBackgroundColor(Color.GRAY);
+        iv.setImageBitmap(bitmap);*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //OpenCV
+    @Override
+    public void onPause() {
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+        super.onPause();
+    }
+
+    // Activityが表示された時の動き
+    @Override
+    protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        // カメラプレビュー開始時に呼ばれる
+        // Mat(int rows, int cols, int type)
+        // rows(行): height, cols(列): width
+        mOutputFrame = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        //mOutputFrame.release();
+        mOutputFrame.release();
+    }
+
+    // フレームをキャプチャする毎(30fpsなら毎秒30回)に呼ばれる
+    @Override
+    public Mat onCameraFrame(Mat inputFrame) {
+        // グレースケール変換
+        Mat grayFrame = new Mat(inputFrame.height(), inputFrame.width(), CvType.CV_8UC1);
+        Imgproc.cvtColor(inputFrame, grayFrame, Imgproc.COLOR_RGB2GRAY);
+        // Cannyフィルタをかける
+        Imgproc.Canny(grayFrame, mOutputFrame, 80, 100);
+        // ビット反転
+        Core.bitwise_not(mOutputFrame, mOutputFrame);
+        return mOutputFrame;
+    }
+}
